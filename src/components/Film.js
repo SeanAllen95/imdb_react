cfe3e777eaa9d9623a2f92cd34842e13a72f006a
Sean <scallen11@btinternet.com>
2023-05-18 06:07:58 +0100

    Needs Urls to work

---
+++ src/components/Film.js
@@ -0,0 +1,11 @@
+const Film = ( {id, name, url}) =>  {
+
+    return (
+        <>
+            <p>{id}</p>
+            <p>{name}</p>
+            <p>{url}</p>
+        </>
+    )
+}
+export default Film
